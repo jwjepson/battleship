b6e6293a0c13e3cf9744fb566a0d7ac96eb28434
Jake Jepson <jwjepson1@gmail.com>
2022-12-04 13:06:43 -0500

    randomly place ships at both vertical and horizontal coords

--- src/app.js
+++ src/app.js
@@ -23,8 +23,14 @@ function GameBoard() {
             return false;                 
         }
         for (let i = 1; i < shipLength; i++) {
-            if (JSON.stringify(coordinates).includes(JSON.stringify([coords[0] + i, coords[1]]))) {
-                return false;
+            if (isHorizontal) {
+                if (JSON.stringify(coordinates).includes(JSON.stringify([coords[0] + i, coords[1]]))) {
+                    return false;
+                }
+            } else {
+                if (JSON.stringify(coordinates).includes(JSON.stringify([coords[0], coords[1] + i]))) {
+                    return false;
+                }
             }
         }
         return true;
@@ -32,25 +38,48 @@ function GameBoard() {
     return {
         getRandomCoordinates(ship) {
             const shipLength = ship.length;
+            isHorizontal = Math.random() > 0.5 ? true : false;
             let x;
             let y;
-            while (true) {
-                x = Math.floor(Math.random() * 10);
-                y = Math.floor(Math.random() * 10);
-                if ((x + ship.length > 10) || (!isValidCoords([x, y], shipLength)))  {
-                    continue;
-                } else {
-                    break;
+            if (isHorizontal) {
+                while (true) {
+                    x = Math.floor(Math.random() * 10);
+                    y = Math.floor(Math.random() * 10);
+                    if ((x + ship.length > 10) || (!isValidCoords([x, y], shipLength)))  {
+                        continue;
+                    } else {
+                        break;
+                    }
                 }
-            }
-
-            ship.coordinates.push([x, y]);
-            coordinates.push([x, y]);
-            for (let i = 1; i < shipLength; i++) {
-                x = x + 1;
+    
                 ship.coordinates.push([x, y]);
                 coordinates.push([x, y]);
+                for (let i = 1; i < shipLength; i++) {
+                    x = x + 1;
+                    ship.coordinates.push([x, y]);
+                    coordinates.push([x, y]);
+                }
+            } else {
+                while (true) {
+                    x = Math.floor(Math.random() * 10);
+                    y = Math.floor(Math.random() * 10);
+                    if ((y + ship.length > 10) || (!isValidCoords([x, y], shipLength)))  {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+    
+                ship.coordinates.push([x, y]);
+                coordinates.push([x, y]);
+                for (let i = 1; i < shipLength; i++) {
+                    y = y + 1;
+                    ship.coordinates.push([x, y]);
+                    coordinates.push([x, y]);
+                }
             }
         }
     }
 }
+
+export {Ship, GameBoard};
