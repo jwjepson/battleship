adfcd9e7909678a315b9f6715546047132005c49
Jake Jepson <jwjepson1@gmail.com>
2022-12-05 12:57:23 -0500

    create event listeners for rotate button & rendering placement of ships

--- src/app.js
+++ src/app.js
@@ -91,4 +91,43 @@ function GameBoard() {
     }
 }
 
+let isHorizontal = false;
+
+const rotateButton = document.querySelector("#rotate-button");
+rotateButton.addEventListener("click", () => {
+    if (isHorizontal == false) {
+        isHorizontal = true;
+    } else {
+        isHorizontal = false;
+    }
+})
+
+const gameSquares = document.querySelectorAll(".square");
+gameSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        gameSquares.forEach((square) => {
+            square.classList.remove("shipHover");
+        });
+
+        let x = parseInt(square.getAttribute("data-coords")[1]);
+        let y = parseInt(square.getAttribute("data-coords")[3]);
+
+        if (isHorizontal) {
+            if (x <= 5) {
+                for (let i = 0; i < 5; i++) {
+                    document.querySelector(`[data-coords='[${x + i},${y}]']`).classList.add("shipHover");
+                }
+            }
+        } else {
+            if (y <= 5) {
+                for (let i = 0; i < 5; i++) {
+                    document.querySelector(`[data-coords='[${x},${y + i}]']`).classList.add("shipHover");
+                }
+            }
+        }
+    })
+})
+
+
+
 export {GameBoard, Ship};
